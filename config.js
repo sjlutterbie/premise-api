5e619491936e59a4770e8a923ba0435f5a49c6fc
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-12-19 15:40:03 +0000

    Fixed local DB links

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/nonprofit-board-match';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/nonprofit-board-match';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/premise-test';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/premise-production';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
