08dbdd46121f929f4f56bf61febddb2444746859
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-12-20 15:11:20 +0000

    Inserted should declaration where necessary

--- test/server.test.js
+++ test/server.test.js
@@ -2,6 +2,7 @@
 
 const chai = require('chai');
   const expect = chai.expect;
+  const should = chai.should();
 const chaiHttp = require('chai-http');
   chai.use(chaiHttp);
 const chaiAsPromised = require('chai-as-promised');
