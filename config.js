132828db23969663c8b7469a6f1023084f5778cc
sjlutterbie <simon.lutterbie@ltrbe.com>
2019-01-10 14:57:06 +0000

    Minor update to config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/premise-test';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/premise-production';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 8081;
 exports.JWT_SECRET = process.env.JWT_SECRET  || 'process.env.JWT_SECRET not loaded';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
